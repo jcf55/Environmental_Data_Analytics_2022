caa3823daf0b6e54410e83be207d424a9ccb755c
jcf55 <jcf55@duke.edu>
2022-03-31 10:23:34 -0400

    Played around with Shiny app and took screen shot of the new formatting!

--- Lessons/11_Part2_Dashboards_RShiny/PeterPaulApp_Full/Fahrenholz.app.R
+++ Lessons/11_Part2_Dashboards_RShiny/PeterPaulApp_Full/Fahrenholz.app.R
@@ -4,6 +4,7 @@ library(shinythemes)
 library(tidyverse)
 
 #### Load data ----
+setwd("C:/Users/Jackie/Desktop/ENV872/Environmental_Data_Analytics_2022/Lessons/11_Part2_Dashboards_RShiny/PeterPaulApp_Full/")
 nutrient_data <- read_csv("Data/NTL-LTER_Lake_Nutrients_PeterPaul_Processed.csv")
 nutrient_data$sampledate <- as.Date(nutrient_data$sampledate, format = "%Y-%m-%d")
 nutrient_data <- nutrient_data %>%
@@ -11,7 +12,7 @@ nutrient_data <- nutrient_data %>%
   select(lakename, sampledate:po4)
 
 #### Define UI ----
-ui <- fluidPage(theme = shinytheme("yeti"),
+ui <- fluidPage(theme = shinytheme("superhero"),
   titlePanel("Nutrients in Peter Lake and Paul Lake"),
   sidebarLayout(
     sidebarPanel(
@@ -20,7 +21,7 @@ ui <- fluidPage(theme = shinytheme("yeti"),
       selectInput(inputId = "y", 
                   label = "Nutrient",
                   choices = c("tn_ug", "tp_ug", "nh34", "no23", "po4"), 
-                  selected = "tp_ug"),
+                  selected = "nh34"),
       
       # Select depth
       checkboxGroupInput(inputId = "fill",
@@ -67,8 +68,8 @@ server <- function(input, output) {
           theme_classic(base_size = 14) +
           scale_shape_manual(values = c(21, 24)) +
           labs(x = "Date", y = expression(Concentration ~ (mu*g / L)), shape = "Lake", fill = "Depth ID") +
-          scale_fill_distiller(palette = "YlOrBr", guide = "colorbar", direction = 1)
-          #scale_fill_viridis_c(option = "viridis", begin = 0, end = 0.8, direction = -1)
+          #scale_fill_distiller(palette = "YlOrBr", guide = "colorbar", direction = 1)
+          scale_fill_viridis_c(option = "viridis", begin = 0, end = 0.8, direction = -1)
       })
        
     # Create a table that generates data for each point selected on the graph  
